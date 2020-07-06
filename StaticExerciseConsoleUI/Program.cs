70555a30a46812b938d097f3b73c293129f4650f
Sel007 <selina.08225.1@gmail.com>
2020-07-06 12:54:58 -0400

    Add project files.

---
+++ StaticExerciseConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExerciseConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the temp converter app!");
+
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            Console.WriteLine($"Celsius {celsius}");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+            Console.WriteLine($"Fahrenheit {fahrenheit}");
+        }
+    }
+}
---
+++ StaticExerciseConsoleUI/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExerciseConsoleUI
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) / 1.8;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius + 32) * 1.8;
+        }
+    }
+    
+}
